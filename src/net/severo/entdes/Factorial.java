c1a98aff49470256acae888508ab6d8c2fdf0869
riekk <115899694+riekk@users.noreply.github.com>
2023-01-25 22:43:14 +0100

    Crear constantes para las String

--- src/net/severo/entdes/Factorial.java
+++ src/net/severo/entdes/Factorial.java
@@ -6,15 +6,18 @@ package net.severo.entdes;
  */
 public class Factorial {
 
+	private static final String VALUE_OF_N2 = "Wert von n!: ";
+	private static final String VALUE_OF_N = "Wert von n: ";
+
 	public static void main(String[] args) {
 
 		int n = (int) (Math.random() * 10);
 		int result = 1;
-		System.out.println("Value of n: " + n);
+		System.out.println(VALUE_OF_N + n);
 		while (n > 1) {
 			result = result * (n - 1);
 		}
-		System.out.println("Value of n!: " + result);
+		System.out.println(VALUE_OF_N2 + result);
 
 	}
 
